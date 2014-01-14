5e31e3dd342d6b3e9b99d1a3a516151027c415aa
Alexander Dudarenko <alexander.dudarenko@abakus.me>
2014-01-14 16:26:17 +0200

    zuka

---
+++ gittest/src/Test0.java
@@ -0,0 +1,20 @@
+// ZUKA
+// 0000000000 11111111111 ZZZZZZZZZZZZZZZZZZZ
+
+public class Test0 {
+
+// zzzzz
+
+    public Test0() {
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
