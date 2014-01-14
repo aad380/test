3c6354ccb80d5ec917d26fac959b189c2cc52595
Alexander Dudarenko <alexander.dudarenko@abakus.me>
2014-01-14 15:47:19 +0200

    aaa

---
+++ gittest/src/Test3.java
@@ -0,0 +1,16 @@
+
+public class Test3 {
+
+    public Test3() {
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
