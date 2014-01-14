062c76777ffb8636f1418e1f8b9f730a0df7041c
Alexander Dudarenko <aad380@gmail.com>
2014-01-14 15:04:07 +0200

    Update Hello.java

--- gittest/src/Hello.java
+++ gittest/src/Hello.java
@@ -1,7 +1,6 @@
-
 public class Hello {
 
     public static void main (String[] args) {
-        System.out.println("Hello World ZZZ\n");
+        System.out.println("Hello World ZZZ 111 222\n");
     }
 }
