3a50f7a0a42b36404b4d3ad856cb409e599bbad1
Alexander Dudarenko <alexander.dudarenko@abakus.me>
2014-01-15 13:25:32 +0200

    sss

---
+++ buka2/zuka2/src/Hello2.java
@@ -0,0 +1,16 @@
+
+public class Hello2 {
+
+    public Hello2() {
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
